001955204204700b17f98848d343e15694913f1c
GForrow <garyforrowbsc@gmail.com>
2020-07-01 16:11:58 +0100

    Working with classes inheritance, polymorphism, abstraction, encapsulation

--- Day3/BirdClass.py
+++ Day3/BirdClass.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+
+
 class Bird:
     fly = True
 
@@ -9,4 +12,44 @@ class Bird:
     def reproduce(self):
         self.babies += 1
 
+    @abstractmethod
+    def eat(self):
+        pass
+
     extinct = False
+
+
+class Owl(Bird):
+
+    def reproduce(self):
+        self.babies += 6
+
+    def eat(self):
+        print("peckpeck")
+
+
+class Dodo(Bird):
+    fly = False
+    extinct = True
+
+    def eat(self):
+        print("nom nom")
+
+    def reproduce(self):
+        if self.extinct == False:
+            self.babies += 1
+        else:
+            print("No more Dodo's")
+
+
+owly = Owl()
+doduo = Dodo()
+
+owly.eat()
+owly.noise()
+owly.reproduce()
+print(owly.babies)
+
+doduo.eat()
+doduo.reproduce()
+
