a958383bd46cb936bf300e2da7312b53a0b8643a
GForrow <garyforrowbsc@gmail.com>
2020-07-01 09:52:45 +0100

    working with files

--- Day2/workingFiles.py
+++
@@ -1,7 +0,0 @@
-file = open("examplefile.txt", "w")
-
-for n in range (1,11):
-    newline = "This is line " + str(n) + "\n"
-    file.write(newline)
-
-file.close()
---
+++ Day3/workingFiles.py
@@ -0,0 +1,15 @@
+file = open("examplefile.txt", "w")
+
+for n in range(1, 11):
+    newline = "This is line " + str(n) + "\n"
+    file.write(newline)
+file.close()
+
+file = open("examplefile.txt", "r")
+
+print("First Line: " + file.readline())
+print("Second Line: " + file.readline())
+print("Rest of file: " + file.read())
+print("Final blank line: " + file.readline())
+
+file.close()
