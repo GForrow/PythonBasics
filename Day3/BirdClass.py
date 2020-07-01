93e74d6ae92de2d9db5aead6b2c90dca61095161
GForrow <garyforrowbsc@gmail.com>
2020-07-01 15:37:44 +0100

    Working with classes

---
+++ Day3/BirdClass.py
@@ -0,0 +1,12 @@
+class Bird:
+    fly = True
+
+    def noise(self):
+        print("Birdnoise")
+
+    babies = 0
+
+    def reproduce(self):
+        self.babies += 1
+
+    extinct = False
--- Day3/StudentClass.py
+++ Day3/StudentClass.py
@@ -1,11 +1,24 @@
 class Student:
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, takenclass):
         self.name = name
         self.age = age
+        self.takenclass = takenclass
+
+    def testscore(self):
+        result = 0
+        finalresult = 0
+        returnresult = 0
+        for i in range(3):
+            score = int(input("What did you score on " + self.takenclass + " test number " + str(i+1) + ": "))
+            result += score
+        finalresult = result / 3
+        output = "Your final score for your " + self.takenclass + " tests was: " + str(round(finalresult, 2)) + "%"
+        return output
 
 
-John = Student("John", 21)
-Jim = Student("Jim", 23)
+John = Student("John", 21, "Tech")
+Jim = Student("Jim", 23, "IT")
 
 print("John is:", getattr(John, "age"))
+print(John.testscore())
